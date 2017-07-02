d63741c7532a11b5ebbde73b82811fccfd885177
Marc Arndt <marc@marcarndt.com>
2017-07-02 19:07:14 +0200

    enabling unit test codacity reporting

---
+++ .delivery/build_cookbook/attributes/default.rb
@@ -0,0 +1,6 @@
+default['coffee-truck']['release']['email'] = 'marc@marcarndt.com'
+default['coffee-truck']['release']['user'] = 'Marc Arndt'
+default['delivery']['config']['truck']['update_dependencies']['include'] = 'com.marcarndt.morse:*'
+default['delivery']['config']['truck']['update_dependencies']['active'] = true
+default['delivery']['config']['truck']['codacy']['upload'] = true
+default['delivery']['config']['truck']['codacy']['token'] = '27c56e9d486742858004476ad6262ce2'
